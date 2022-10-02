40f3efee9aae04a84a6448043945846a7cf6637f
Said Herrera <sherrerab@rentingcolombia.com>
2022-10-02 17:27:50 -0500

    se agrega consumo Dynamo y nuevos endpoints

--- application/src/main/java/com/appgate/application/controller/ApplicationController.java
+++ application/src/main/java/com/appgate/application/controller/ApplicationController.java
@@ -1,9 +1,12 @@
 package com.appgate.application.controller;
 
+import com.appgate.application.dynamo.service.EmployeeService;
+import com.appgate.application.entity.EmployeeDyDB;
 import com.appgate.application.interfaces.EmployeeRepository;
 import com.appgate.application.kafka.Producer;
 import com.appgate.application.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +17,9 @@ public class ApplicationController {
     private final Producer producer;
     @Autowired
     private EmployeeRepository employeeRepository;
+    @Autowired
+    EmployeeService employeeService;
+
     @Autowired
     public ApplicationController(Producer producer){
         this.producer=producer;
@@ -28,5 +34,32 @@ public class ApplicationController {
     @GetMapping("/views")
     public List<Employee> views(@RequestParam("month") Integer month,@RequestParam("salary") Integer salary){
         return employeeRepository.findAllBySalaryAndMonth(month,salary);
+    }
+
+    @GetMapping(value = "/getemployee")
+    public ResponseEntity<List<EmployeeDyDB>> getProducts()
+    {
+        try
+        {
+            return new ResponseEntity<>(employeeService.getAllEmployee(), HttpStatus.OK);
+        }
+        catch (Exception e)
+        {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping(value = "saveemployee", consumes = "application/json")
+    public ResponseEntity saveProduct(@RequestBody EmployeeDyDB employeeDyDB)
+    {
+        try
+        {
+            employeeService.saveProduct(employeeDyDB);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        catch (Exception e)
+        {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
--- application/src/main/java/com/appgate/application/kafka/Consumer.java
+++ application/src/main/java/com/appgate/application/kafka/Consumer.java
@@ -1,6 +1,8 @@
 package com.appgate.application.kafka;
 
+import com.appgate.application.dynamo.service.EmployeeService;
 import com.appgate.application.entity.Employee;
+import com.appgate.application.entity.EmployeeDyDB;
 import com.appgate.application.entity.ReqEmp;
 import com.appgate.application.interfaces.EmployeeRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -20,19 +22,29 @@ import org.springframework.stereotype.Service;
 public class Consumer {
     @Autowired
     private EmployeeRepository employeeRepository;
+    @Autowired
+    EmployeeService employeeService;
+
     @KafkaListener(topics = "my_topic",groupId = "group_id")
     public void consumerMessage(String body){
         System.out.println(String.format("Message received -> %s", body.toString()));
         ObjectMapper mapper=new ObjectMapper();
         try {
             ReqEmp employee=mapper.readValue(body,ReqEmp.class);
+            EmployeeDyDB employeeDyDB=new EmployeeDyDB();
+            employeeDyDB.setNombre(employee.payload.getNombre());
+            employeeDyDB.setMeses(employee.payload.getMeses());
+            employeeDyDB.setSalario(employee.payload.getSalario());
+            employeeService.saveProduct(employeeDyDB);
             employeeRepository.save(employee.payload);
 
         } catch (JsonProcessingException e) {
             e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-     }
+    }
 
 
 }
